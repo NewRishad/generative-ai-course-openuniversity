102e57041fc875736580ea93dc8e06f6c3c87a49
NewRishad <rishadhussein90@gmail.com>
2025-11-25 22:03:35 +0300

    feat: simple one component app

--- Tutorials/zero-to-hero-tutorials/jac_client/todo-app/src/app.js
+++ Tutorials/zero-to-hero-tutorials/jac_client/todo-app/src/app.js
@@ -1,12 +1,9 @@
 import {__jacJsx, __jacSpawn} from "@jac-client/utils";
 import { useState, useEffect } from "react";
-function app() {
-  let [count, setCount] = useState(0);
-  useEffect(() => {
-    console.log("Count: ", count);
-  }, [count]);
-  return __jacJsx("div", {}, [__jacJsx("h1", {}, ["Hello, World!"]), __jacJsx("p", {}, ["Count: ", count]), __jacJsx("button", {"onClick": e => {
-    setCount(count + 1);
-  }}, ["Increment"])]);
+function TodoItem(props) {
+  return __jacJsx("div", {}, [__jacJsx("input", {"type": "checkbox", "checked": props.done}, []), __jacJsx("span", {}, [props.text]), __jacJsx("button", {}, ["Delete"])]);
 }
-export { app };
+function app() {
+  return __jacJsx("div", {}, [__jacJsx("h1", {}, ["My Todos"]), __jacJsx(TodoItem, {"text": "Learn Jac basics", "done": true}, []), __jacJsx(TodoItem, {"text": "Build a todo app", "done": false}, []), __jacJsx(TodoItem, {"text": "Deploy to production", "done": false}, [])]);
+}
+export { TodoItem, app };
